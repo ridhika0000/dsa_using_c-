ed1162b35aa8ac9f17f549c75b835bdfa7c92a2b
ridhika0000 <ridhikamanocha@gmail.com>
2025-04-16 12:04:47 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    c = a + b;
+
+    cout << "Sum of two numbers: " << c << endl;
+
+    return 0;
+}
